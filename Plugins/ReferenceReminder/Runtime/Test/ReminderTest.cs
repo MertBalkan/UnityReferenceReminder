8c617529e598061d3bed766b192777b3eda8eca5
Mert Balkan <mert__balkan@hotmail.com>
2023-08-08 19:38:48 +0300

    Update ReminderTest.cs

--- Plugins/ReferenceReminder/Runtime/Test/ReminderTest.cs
+++ Plugins/ReferenceReminder/Runtime/Test/ReminderTest.cs
@@ -5,7 +5,8 @@ public class ReminderTest : MonoBehaviour
     [RefReminder("Scene assign")] public GameObject reminderTest;
     [RefReminder("Doesn't matter whether assigned or not!")] public GameObject reminderTest1;
     [RefReminder] public GameObject reminderTest2;
-    [RefReminder("Change string")] public string mString;
+    
+    public string mString;
     public float mFloat;
     public int mInt;
 }
